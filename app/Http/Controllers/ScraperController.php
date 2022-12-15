f446bb46c14fbea730eba46986b5c00bfe89e51c
Emil Sležis <emilslezis@gmail.com>
2022-12-15 15:36:53 +0200

    Joprojam scrapoju

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -1,26 +1,25 @@
 <?php
+
 namespace App\Http\Controllers;
-use Goutte\Client;
-use Illuminate\Http\Controllers;
+
+use Illuminate\Http\Request;
+
 class ScraperController extends Controller
-
 {
-    private $results = array();
-    public function scraper()
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
     {
-        $client = new Client();
-        $url = 'https://www.worldometers.info/coronavirus/';
-        $page = $client->request('GET', $url);
-       //echo "<pre>";
-       //print_r($page);
-
-       //echo $page->filter( selector: '.maincounter-number')->text();
-       $page->filter( selector: '#maincounter-wrap')->each(function($item){
-        $this-> results[$item->filter('h1')->text()]=$item->filter('maincounter-number')->text();
-       });
-
-       return $this->results;
-        //return view(view:'scraper');
+        return view('scraper');
     }
 }
-?>
--- resources/views/scraper.blade.php
+++ resources/views/scraper.blade.php
@@ -1,1 +1,10 @@
-scraper
+<?php
+
+require 'vendor/autoload.php';
+use Goutte\Client;
+$client = new Client();
+$url = "https://www.bbc.com/news/topics/cgdzpg5yvdvt/stock-markets";
+$crawler = $client->request('GET', $url);
+$news = $crawler->filter("<headline's selector>")->text();
+echo($news."\n");
+?>
